fb5fa8c41d9f912a1015a7b4e938bd8b420eb8d9
Marko Colic <markocolic@Markos-MacBook-Pro.local>
2019-02-01 09:35:36 +0100

    Deleted second starter

--- starter/src/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Create and shuffle a new deck of cards
-  const deck = new Deck();
-  deck.shuffle();
-  console.log(deck);
-});
--- starter/src/assets/js/card.js
+++
@@ -1,9 +0,0 @@
-/*
- * A playing card
- */
-class Card {
-  constructor(rank, suit) {
-    this.rank = rank;
-    this.suit = suit;
-  }
-} 
